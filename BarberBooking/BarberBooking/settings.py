0b816b258cf9b41d568b3e0b9f1e294a89a00da8
judahtc <84022158+judahtc@users.noreply.github.com>
2022-08-24 11:28:09 +0200

    try

--- BarberBooking/BarberBooking/settings.py
+++ BarberBooking/BarberBooking/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 django_heroku.settings(locals())
 
 # Default primary key field type
