085deef2d811edce471227981c24aaf1c7dccf95
Tawanda M <tmnyoni@outlook.com>
2022-08-25 08:51:39 +0200

    minor url changes.

--- barbers/serializers.py
+++ barbers/serializers.py
@@ -1,5 +1,6 @@
-from . import models
 from rest_framework import serializers
+from . import models
+
 
 class BarberSerializer(serializers.ModelSerializer):
     class Meta:
--- barbers/urls.py
+++ barbers/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-path('login',views.LoginView.as_view(),name="login"),
-path('barbers',views.BarberView.as_view(),name="barber"),
-path('barber',views.OneBarberView.as_view(),name="updatebarber"),
+path('login/',views.LoginView.as_view(),name='login'),
+path('barbers/',views.BarberView.as_view(),name='barbers'),
+path('barber/',views.OneBarberView.as_view(),name='updatebarber'),
 ]
